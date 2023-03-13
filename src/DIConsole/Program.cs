16728f257453ec78291d040ab0db4edb0fb0f230
seanr89 <srafferty89@gmail.com>
2023-03-13 21:27:31 +0000

    added config and logging controls

--- src/DIConsole/Program.cs
+++ src/DIConsole/Program.cs
@@ -2,13 +2,24 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+    
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddSingleton<MyInjectedClass>();
         services.AddSingleton<Main>();
         services.AddHostedService<MyHostedService>();
+    })
+        .ConfigureLogging((context, logging) => {
+        logging.AddConfiguration(context.Configuration.GetSection("Logging"));
     }).Build();
 
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
